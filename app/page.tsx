8a4aaefec2c209f58dea6ce39aa2606d6f899071
Nguyen Huu Huy <huynh@vietis.vn>
2023-09-29 17:29:25 +0700

    feat: add line 3x3 & highlight on selected cell

--- app/page.tsx
+++ app/page.tsx
@@ -15,16 +15,31 @@ export default function Home() {
   const [solvedMatrix, setSolvedMatrix] = useState<number[][]>();
   const [extremeMatrix, setExtremeMatrix] = useState<IMappingMatrix[][]>();
 
+  const [selectedIndex, setSelectedIndex] = useState<number[]>();
+
+  const handleClickCell = (
+    cell: IMappingMatrix,
+    rowIndex: number,
+    colIndex: number
+  ) => {
+    // add background color for cell
+    setSelectedIndex([rowIndex, colIndex]);
+
+    // highlight value in cell and same value in another cells
+    if (!cell.value) {
+      return;
+    }
+  };
+
   useEffect(() => {
     const generateMatrix = generateRandomSudokuMatrix();
-    console.log(1232, generateMatrix);
     const extremeMode = removeExtremeMode(cloneDeep(generateMatrix));
     const mappingExtremeMatrix = mappingMatrix(cloneDeep(extremeMode));
 
     setTimeout(() => {
       setSolvedMatrix(generateMatrix);
       setExtremeMatrix(mappingExtremeMatrix);
-    }, 0);
+    }, 1000);
   }, []);
 
   if (!extremeMatrix) {
@@ -38,14 +53,27 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <div className="grid grid-cols-9 grid-rows-9 border-2 border-gray-500">
-        {extremeMatrix.map((it, idx) => (
-          <div className={`sudoku-row`} key={idx}>
-            {it.map((cell, index) => (
+        {extremeMatrix.map((it, rowIndex) => (
+          <div key={rowIndex}>
+            {it.map((cell, colIndex) => (
               <div
-                className={`sudoku-cell w-16 h-16 flex flex-col justify-center items-center border border-sky-300 text-3xl cursor-pointer `}
-                key={index}
+                className={`w-16 h-16 flex flex-col justify-center items-center border border-sky-300 text-3xl text-neutral-700 cursor-pointer relative ${
+                  selectedIndex?.[0] === rowIndex &&
+                  selectedIndex[1] === colIndex &&
+                  "bg-blue-200 text-red-500"
+                }`}
+                key={colIndex}
+                onClick={() => handleClickCell(cell, rowIndex, colIndex)}
               >
                 {cell.value !== 0 && cell.value}
+
+                {/* line  to separate 3x3 */}
+                {[3, 6].includes(rowIndex + 1) && (
+                  <div className="absolute w-16 h-16 top-0 border-r-2 border-gray-500 z-10" />
+                )}
+                {[3, 6].includes(colIndex + 1) && (
+                  <div className="absolute w-16 h-16 top-0 border-b-2 border-gray-500 z-10" />
+                )}
               </div>
             ))}
           </div>
